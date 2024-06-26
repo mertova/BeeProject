15cc3230c5c5693a4ec5f5c4b4fa395f589b98f4
Lukrecia Mertova <lmertova12@gmail.com>
2024-06-26 12:29:49 +0200

    ad tests and load for form analysis

--- src/form_analysis.py
+++ src/form_analysis.py
@@ -1,8 +1,32 @@
 from pathlib import Path
-
 import cv2
+from pdf2image import convert_from_path
 
 from image_processing.reference import Reference
+
+
+
+# todo test on .pdf
+def load(path: Path = None):
+    """
+    Load image from the template path or the image. Fill in all the parameters of Template class.
+    """
+    if path is None or not path.is_file():
+        print("path is not a directory of a file")
+        exit(1)
+
+    if path.suffix.endswith('.pdf'):
+        images = convert_from_path(path)
+        if len(images) != 1:
+            raise FileExistsError("pdf file " + path.as_posix() + "has several pages. 1 PDF page is "
+                                                                  "required.")
+        img = images[0]
+    elif path.suffix.endswith('.jpg') or path.suffix.endswith('.png'):
+        img = cv2.imread(path.as_posix())
+    else:
+        raise FileNotFoundError("Unsupported file type for reference file")
+
+    return img
 
 
 class FormAnalysis:
@@ -25,9 +49,10 @@ class FormAnalysis:
 
         if averaging:
             self.averaging(data_sample_dir, limit, transform)
+        averaged = self.reference.get_grey()
         self.reference.clean_averaged_form()
         if debug:
-            self._dump_debug_images([pen_eliminated, self.reference.get_grey()])
+            self._dump_debug_images([averaged, pen_eliminated, self.reference.get_grey()])
         else:
             cv2.imwrite((self.out_dir / "mask.png").as_posix(), self.reference.get_color())
         return self.reference.get_grey()
@@ -44,7 +69,6 @@ class FormAnalysis:
                     scan = self.reference.map_img_to_ref(scan)
                 self.reference.add_weighted(scan)
                 i += 1
-
         print("cleaning form ...")
 
     def _dump_debug_images(self, images):
--- test/form_analysis_test.py
+++ test/form_analysis_test.py
@@ -3,22 +3,20 @@ from pathlib import Path
 
 import cv2
 
-import execute_extraction
 from form_analysis import FormAnalysis
 
 
 class IdentificationTest(unittest.TestCase):
-
     def test_analysis_form1(self):
         # set
         data_samples = Path('./scans/Niedersach_examples_10/')
         reference_path = Path('./scans/Niedersach_examples_10/Sample_Niedersachsen-2-1.png')
-        output_path = Path('./results/5/')
+        # edit if new results should be generated
+        output_path = Path('./results/7/')
         output_path.mkdir(parents=True, exist_ok=True)
+        reference = cv2.imread(reference_path.as_posix())
 
         # run
-        reference = cv2.imread(reference_path.as_posix())
-        t = type(reference)
         form_analysis = FormAnalysis(output_path, reference)
         form_analysis.extract(data_samples, 5, True, True, True)
 
@@ -36,6 +34,7 @@ class IdentificationTest(unittest.TestCase):
         # set
         data_samples = Path('./scans/Niedersach_transformed_10/')
         reference_path = Path('./scans/Niedersach_examples_10/Sample_Niedersachsen-2-1.png')
+        # edit if new results should be generated
         output_path = Path('./results/5/')
         output_path.mkdir(parents=True, exist_ok=True)
 
@@ -57,13 +56,12 @@ class IdentificationTest(unittest.TestCase):
     def test_analysis_form2(self):
         # set
         data_samples = Path('./scans/Form2-examples-51-png/')
-        reference_path = Path('./scans/Niedersach_examples_10/Sample_Niedersachsen-2-1.png')
-        output_path = Path('./results/5/')
+        reference_path = Path('./scans/Form2-examples-51/Sample_Form2-1.png')
+        output_path = Path('./results/7/')
         output_path.mkdir(parents=True, exist_ok=True)
 
         # run
         reference = cv2.imread(reference_path.as_posix())
-        t = type(reference)
         form_analysis = FormAnalysis(output_path, reference)
         form_analysis.extract(data_samples, 5, True, True, True)
 
@@ -76,30 +74,6 @@ class IdentificationTest(unittest.TestCase):
             cv2.imshow(x.name, cv2.imread(x.as_posix()))
             cv2.waitKey(0)
             i += 1
-
-    def test_analysis(self):
-        data_samples = Path('./scans/Niedersach_examples_10/').as_posix()
-        reference = Path('./scans/Niedersach_examples_10/Sample_Niedersachsen-2-1.png').as_posix()
-        output_path = Path('./results/1/')
-        output_path.mkdir(parents=True, exist_ok=True)
-        template, grid = execute_extraction.main(path_t_output=output_path.as_posix(), path_reference=reference,
-                                                 eps_h=20, eps_v=15, template_extraction=True,
-                                                 path_data_sample=data_samples, data_limit=15, transform=True,
-                                                 debug=True)
-        self.assertIsNotNone(template)
-        self.assertIsNotNone(grid)
-        print(template)
-        print(grid)
-
-    def test_no_table_extraction(self):
-        reference = Path('../data/form1/form1-manual-clean.png').as_posix()
-        output_path = Path('./results/2/')
-        output_path.mkdir(parents=True, exist_ok=True)
-        grid = execute_extraction.main(path_t_output=output_path.as_posix(), path_reference=reference,
-                                       eps_h=10, eps_v=20, template_extraction=False,
-                                       debug=True)
-        self.assertIsNotNone(grid)
-        print(grid)
 
 
 if __name__ == '__main__':
